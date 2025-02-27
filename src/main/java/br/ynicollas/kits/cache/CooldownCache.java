8b8b6fb0557538367d0d4f0407d277ffa4264fa7
Nicolas <nicolas2006.capela@gmail.com>
2025-02-26 21:06:38 -0300

    used ConcurrentHashMap to ensure thread safety

--- src/main/java/br/ynicollas/kits/cache/CooldownCache.java
+++ src/main/java/br/ynicollas/kits/cache/CooldownCache.java
@@ -4,27 +4,26 @@ import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class CooldownsCache {
-    private final Map<String, Map<String, Long>> cooldowns = new HashMap<>();
+public class CooldownCache {
+
+    private final Map<String, Map<String, Long>> cooldowns = new ConcurrentHashMap<>();
 
     public void addCooldown(Player player, String kitId, long duration) {
-        cooldowns.computeIfAbsent(player.getName(), key -> new HashMap<>()).put(kitId, System.currentTimeMillis() + duration);
+        cooldowns.computeIfAbsent(player.getName(), key -> new ConcurrentHashMap<>())
+                .put(kitId, System.currentTimeMillis() + duration);
     }
 
     public void removeCooldown(Player player, String kitId) {
-        if (cooldowns.containsKey(player.getName())) {
-            cooldowns.get(player.getName()).remove(kitId);
-            if (cooldowns.get(player.getName()).isEmpty()) {
-                cooldowns.remove(player.getName());
-            }
-        }
+        cooldowns.computeIfPresent(player.getName(), (name, kitMap) -> {
+            kitMap.remove(kitId);
+            return kitMap.isEmpty() ? null : kitMap;
+        });
     }
 
     public void removeCooldownsForKit(String kitId) {
-        for (Map<String, Long> cooldowns : cooldowns.values()) {
-            cooldowns.remove(kitId);
-        }
+        cooldowns.forEach((name, kitMap) -> kitMap.remove(kitId));
     }
 
     public boolean hasCooldown(Player player, String kitId) {
