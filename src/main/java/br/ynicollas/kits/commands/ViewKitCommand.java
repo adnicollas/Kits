6b0ec59cf307e3de9c7ee4e743665e759ead9d52
Nicolas <nicolas2006.capela@gmail.com>
2025-02-27 01:12:45 -0300

    new command

---
+++ src/main/java/br/ynicollas/kits/commands/ViewKitCommand.java
@@ -0,0 +1,53 @@
+package br.ynicollas.kits.commands;
+
+import br.ynicollas.kits.model.Kit;
+import br.ynicollas.kits.storage.kits.KitsStorage;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class ViewKitCommand implements CommandExecutor {
+
+    private final KitsStorage kits;
+
+    public ViewKitCommand(KitsStorage kits) {
+        this.kits = kits;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Apenas jogadores podem utilizar este comando.");
+            return false;
+        }
+
+        Player player = (Player) sender;
+
+        if (args.length != 1) {
+            player.sendMessage(ChatColor.RED + "Uso correto: /editarkit <id>");
+            return false;
+        }
+
+        String id = args[0].toLowerCase();
+
+        Kit kit = kits.getKit(id);
+
+        if (kit == null) {
+            player.sendMessage(ChatColor.RED + "O kit '" + id + "' não existe.");
+            return false;
+        }
+
+        Inventory kitInventory = player.getServer().createInventory(null, 54, ChatColor.DARK_GRAY + "Visualizando");
+
+        if (kit.getItems() != null) {
+            kitInventory.setContents(kit.getItems());
+        }
+
+        player.openInventory(kitInventory);
+
+        return true;
+    }
+}
