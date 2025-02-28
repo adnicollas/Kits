3ee21a6d5a772e0899d421950f169cb67ce44aaf
Nicolas <nicolas2006.capela@gmail.com>
2025-02-27 22:29:20 -0300

    accert ItemStack[] is non null

--- src/main/java/br/ynicollas/kits/models/Kit.java
+++ src/main/java/br/ynicollas/kits/models/Kit.java
@@ -2,19 +2,23 @@ package br.ynicollas.kits.models;
 
 import lombok.Getter;
 
+import lombok.NonNull;
 import lombok.Setter;
 import org.bukkit.inventory.ItemStack;
 
 @Getter
 public class Kit {
+
     private final String id;
     private final String permission;
+
     private final KitCooldown cooldown;
 
     @Setter
+    @NonNull
     private ItemStack[] items;
 
-    public Kit(String id, String permission, KitCooldown cooldown, ItemStack[] items) {
+    public Kit(String id, String permission, KitCooldown cooldown, @NonNull ItemStack[] items) {
         this.id = id;
         this.permission = permission;
         this.cooldown = cooldown;
